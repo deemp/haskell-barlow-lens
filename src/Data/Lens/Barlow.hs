af15e367bcdcf3365819af7e3daa11b8730ef92c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-16 01:14:33 +0300

    add helpers

--- src/Data/Lens/Barlow.hs
+++ src/Data/Lens/Barlow.hs
@@ -1,1 +1,18 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Data.Lens.Barlow where
+
+import Control.Lens (Optic)
+import Data.Lens.Barlow.Construction ( ConstructBarlow(..) )
+import Data.Lens.Barlow.Parser (Parse)
+
+barlow :: forall path {p} {f} {s} {t} {a} {b}. (ConstructBarlow (Parse path) p f s t a b) => Optic p f s t a b
+barlow = constructBarlow @(Parse path)
+
+bw :: forall path {p} {f} {s} {t} {a} {b}. (ConstructBarlow (Parse path) p f s t a b) => Optic p f s t a b
+bw = barlow @path
